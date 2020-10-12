ad6789a5ef0aa00c0856cb3b441e93aa9f2707b9
Maheswari99-gif <71683400+Maheswari99-gif@users.noreply.github.com>
2020-10-13 00:01:04 +0530

    Update TestMaximum.java

--- TestMaximum.java
+++ TestMaximum.java
@@ -7,7 +7,7 @@ public class TestMaximum {
 
 	private static final Logger log = LogManager.getLogger(TestMaximum.class);
 
-	public Integer toMax(Integer x, Integer y, Integer z) {
+	public Integer findMax(Integer x, Integer y, Integer z) {
 		Integer max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
@@ -16,7 +16,7 @@ public class TestMaximum {
 
 		return max;
 	}
-	public Float toMax(Float x, Float y, Float z) {
+	public Float findMax(Float x, Float y, Float z) {
 		Float max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
@@ -25,7 +25,7 @@ public class TestMaximum {
 
 		return max;
 	}
-	public String toMax(String x, String y, String z)
+	public String findMax(String x, String y, String z)
 	{
 		String max = x;
 		if(y.compareTo(max)>0)
@@ -41,9 +41,9 @@ public class TestMaximum {
 		Integer a = 40, b = 20, c = 25;
 		Float d=10.0f, e=15.f, f=31.f;
 		String g="200",h="357",i="870";
-		Integer maxInt = testMaximum.toMax(a, b, c);
-		Float maxFloat=testMaximum.toMax(d, e, f);
-		String maxString=testMaximum.toMax(g, h, i);
+		Integer maxInt = testMaximum.findMax(a, b, c);
+		Float maxFloat=testMaximum.findMax(d, e, f);
+		String maxString=testMaximum.findMax(g, h, i);
 		log.info("The maximum integer in " + a + ", " + b + ", " + c + " is " + maxInt);
 		log.info("The maximum float in "+d+", "+e+", "+f+" is "+maxFloat);
 		log.info("The maximum string in "+g+", "+h+", "+i+" is "+maxString);
