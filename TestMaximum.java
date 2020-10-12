813273b4cf1509d7992a2338bf954880dd488af8
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-12 22:53:49 +0530

    MaheswariKoyyani : Maximum of 3 float numbers

--- TestMaximum.java
+++ TestMaximum.java
@@ -16,11 +16,23 @@ public class TestMaximum {
 
 		return max;
 	}
+	public Float toMax(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(max) > 0)
+			max = y;
+		if (z.compareTo(max) > 0)
+			max = z;
+
+		return max;
+	}
 
 	public static void main(String[] args) {
 		TestMaximum testMaximum = new TestMaximum();
 		Integer a = 40, b = 20, c = 25;
+		Float d=10.0f, e=15.f, f=31.f;
 		Integer maxInt = testMaximum.toMax(a, b, c);
+		Float maxFloat=testMaximum.toMax(d, e, f);
 		log.info("The maximum integer in " + a + ", " + b + ", " + c + " is " + maxInt);
+		log.info("The maximum float in "+d+", "+e+", "+f+" is "+maxFloat);
 	}
 }
