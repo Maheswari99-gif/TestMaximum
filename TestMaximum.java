fcdd3de0822575be1a55f49d219fc2eeab90af6c
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-12 23:34:57 +0530

    MaheswariKoyyani : Refractor to use 3 variables

--- TestMaximum.java
+++ TestMaximum.java
@@ -3,11 +3,27 @@ package com.capgemini.testmaximum;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class TestMaximum {
+public class TestMaximum <T extends Comparable<T>> {
 
 	private static final Logger log = LogManager.getLogger(TestMaximum.class);
 
-	public <T extends Comparable<T>> T findMax(T x, T y, T z)
+T x, y, z;
+	
+	public TestMaximum(T x, T y, T z)
+	{
+		super();
+		this.x=x;
+		this.y=y; 
+		this.z=z;
+	}
+	
+	public T findMax()
+	{
+		return  TestMaximum.findMax(this.x,this.y,this.z);
+		
+	}
+	
+	public static <T extends Comparable<T>> T findMax(T x, T y, T z)
 	{
 		T max = x;
 		if(y.compareTo(max)>0)
@@ -17,10 +33,22 @@ public class TestMaximum {
 	
 		return max;
 	}
-	public static void main(String[] args) {
-		TestMaximum testMaximum = new TestMaximum();
-		String a="40", b="20", c="55";
-		String maxString=testMaximum.findMax(a, b, c);
-		log.info("The maximum integer in "+a+", "+b+", "+c+" is "+maxString);
-	}
+	
+	
+	
+    public static void main( String[] args )
+    {
+    	
+		Integer a=10, b=30, c=50;
+		TestMaximum<Integer>testMaximum=new TestMaximum<Integer>(a,b,c);
+		String d="5", e="56", f="25";
+		TestMaximum<String>testMaximum1=new TestMaximum<String>(d,e,f);
+		Float g=10.5f, h=30.f, i=50.8f;
+		TestMaximum<Float>testMaximum2=new TestMaximum<Float>(g,h,i);
+		log.info("The maximum in "+a+", "+b+", "+c+" is "+testMaximum.findMax());
+		log.info("The maximum in "+d+", "+e+", "+f+" is "+testMaximum1.findMax());
+		log.info("The maximum in "+g+", "+h+", "+i+" is "+testMaximum2.findMax());
+		
+    }
 }
+
